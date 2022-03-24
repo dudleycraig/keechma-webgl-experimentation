f03cc9743fb8991598df46635867b098c6f2cf55
dpoljak <poljak5@gmail.com>
2022-03-24 15:10:29 +0100

    Stop the controller from breaking

--- src/cljs/main/controllers/timeout.cljs
+++ src/cljs/main/controllers/timeout.cljs
@@ -11,54 +11,67 @@
             [keechma.next.controllers.dataloader :as controllers-dataloader]
             [keechma.pipelines.core :as pipelines :refer-macros [pipeline!]]
             [com.verybigthings.state-machete.core :as state-machete]
-            
+
             [main.lib.fsm :refer [handle register]]))
 
 (derive :timeout ::controllers-pipelines/controller)
 
-(def fsm
-  [:state-machete/state#timeout
+(def state-machine
+  [:fsm/root
 
-   [:state-machete/state#inert
-    [:state-machete/transition
-     #:state-machete.transition
-     {:event :on-click
-      :target :active
-      :state-machete/on (fn [fsm {:keys [data]}]
-                          (state-machete/assoc-in-data fsm [:status] data))}]]
+   [:fsm/state#inert
+    [:fsm/transition
+     #:fsm.transition {:event :on-click
+                       :target :b}]]
 
-   [:state-machete/state#active
-    [:state-machete.on/enter (fn [fsm {:keys [data]}]
-                               (let [payload (state-machete/get-data fsm)]
-                                 (state-machete/send fsm {:state-machete/event ::active})))]]
+   [:fsm/state#b
+    {:fsm.on/enter #(println "B")}
+    [:fsm/transition #:fsm.transition {:event :on-click :target :c}]]
 
-   [:state-machete/state#success
-    [:state-machete.on/enter (fn [fsm {:keys [data]}] 
-                               ;; transition inert state
-                               )]]])
+   [:fsm/state#c
+    {:fsm.on/enter #(println "C")}]]
+
+  #_[:fsm/root
+     [:fsm/state#inert
+      [:fsm/transition
+       #:fsm.transition
+        {:event :on-click
+         :target :active
+         :fsm/on (fn [fsm {:keys [data]}]
+                   (println "foo")
+                   #_(state-machete/assoc-in-data fsm [:status] data))}]]
+
+     [:fsm/state#active
+      [:fsm.on/enter (fn [fsm {:keys [data]}]
+                       (let [payload (state-machete/get-data fsm)]
+                         (state-machete/send fsm {:fsm/event ::active})))]]
+
+     [:fsm/state#success
+      [:fsm.on/enter (fn [fsm {:keys [data]}]
+                       (println "here" data))]]])
 
 (defmethod keechma-controller/prep :timeout [controller]
   (register
    controller
    {:on-click
     (pipeline! [payload {:keys [state*]}]
-      (pipelines/swap! state* (fn [state payload] (assoc state :status payload)) payload)
-      "active")
+               (pipelines/swap! state* (fn [state payload] (assoc state :status payload)) payload)
+               "active")
 
-    ::active
-    (pipeline! [payload {:keys [state*]}]
-      (pipelines/swap! state* (fn [state payload] (assoc state :status payload)))
-      (promesa/delay 3000)
-      "success")
+    #_(::active
+       (pipeline! [payload {:keys [state*]}]
+                  (pipelines/swap! state* (fn [state payload] (assoc state :status payload)))
+                  (promesa/delay 3000)
+                  "success")
 
-    ::success
-    (pipeline! [payload {:keys [state*]}]
-      (pipelines/swap! state* (fn [state payload] (assoc state :status payload)))
-      (promesa/delay 750)
-      "inert")}))
+       ::success
+       (pipeline! [payload {:keys [state*]}]
+                  (pipelines/swap! state* (fn [state payload] (assoc state :status payload)))
+                  (promesa/delay 750)
+                  "inert"))}))
 
 (defmethod keechma-controller/start :timeout [_ _ _]
-  (state-machete/start {} (state-machete/compile fsm)))
+  (state-machete/start (state-machete/compile state-machine) {}))
 
 (defmethod keechma-controller/handle :timeout [controller event payload]
   (handle controller event payload))
